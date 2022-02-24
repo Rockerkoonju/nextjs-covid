9c02a9b9bb0c3691c07e54d45f9a467f989867fd
Rockerkoonju <78637372+Rockerkoonju@users.noreply.github.com>
2022-02-24 08:20:52 +0700

    add-next-progressbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import NextNProgress from "nextjs-progressbar";
+import Navbar from '../components/Nav';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return <>
+  <Navbar />
+  <NextNProgress
+  color="#29D"
+  startPosition={0.3}
+  stopDelayMs={200}
+  height={10}
+  showOnShallow={true}
+/>
+  <Component {...pageProps} /> 
+  <Footer />
+  </>
 }
 
 export default MyApp
