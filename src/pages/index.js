4f9a9b366c5d701bf08e15b87729ec0a08f7a601
Rockerkoonju <78637372+Rockerkoonju@users.noreply.github.com>
2022-02-24 08:21:13 +0700

    change to ssr

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,80 +1,110 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 // import styles from '../styles/Home.module.css'
-import { useState } from 'react'
-import Layout from '../components/Layout'
-import SearchInput from '../components/SearchInput'
-import ProvincesTable from '../components/ProvincesTable'
+import { useState, useEffect } from "react";
+import Layout from "../components/Layout";
+import SearchInput from "../components/SearchInput";
+import ProvincesTable from "../components/ProvincesTable";
 
-export default function Home({provinces, country}) {
-  const [keyword, setKeyword] = useState("")
+export default function Home({ country }) {
+  const [keyword, setKeyword] = useState("");
 
-  // console.log(country)
-  
-  const filteredProvinces = provinces.filter(
-    (province) =>
-      province.province.toLowerCase().includes(keyword)
+  const [name, setName] = useState("")
+
+  const dataLength = country.length - 1;
+  const lastDate = country[dataLength].txn_date;
+  // const today_case = country.find(data => data === today)
+
+  // const filteredDate = setKeyword.filter(
+  //   (setKeyword) =>
+  //   setKeyword.protxn_datevince.toLowerCase().includes(keyword)
+  // );
+
+  const filteredDate = country.find(
+    (data) => data.txn_date === keyword || data.txn_date === lastDate
   );
-
-  // console.log(provinces)
+  // console.log(filteredDate);
 
   const onInputChange = (e) => {
     e.preventDefault();
+    const input = e.target.value;
 
-    // console.log(e.target.value)
+    if (input > lastDate) {
+      setKeyword(lastDate);
+    } else {
+      setKeyword(input);
+    }
+  };
 
-    setKeyword(e.target.value.toLowerCase())
-  }
+  useEffect(() => {
+    setKeyword(lastDate);
+  }, []);
 
   return (
     <Layout>
-      <div className="mb-5 md:flex md:justify-between">
-        <div className="my-3 mx-0 text-primary md:flex-1">Found {provinces.length} Provinces</div>
-
-        <div className="md:flex-[2]">
-          <SearchInput
-            placeholder="Filter by Province"
-            onChange={onInputChange}
-          />
-        </div>
+      <div className=" w-[650px] m-[0_auto] p-10 bg-slate-300 rounded-xl shadow-md">
+        <div className="flex md:justify-end justify-center">
         
+        <input
+        className=" text-neutral-900 border-4 rounded-lg border-sky-300  flex "
+        type="date"
+        name="date"
+        value={keyword}
+        onChange={onInputChange}
+      />
       </div>
-        <div className="flex justify-end my-2">Update At {provinces[0].update_date}</div>
-      <ProvincesTable provinces={filteredProvinces} />
+      
+    <div className="flex justify-center">
+      <div className="grid grid-rows-4 gap-2 grid-flow-col my-5">
+        {/* new cases */}
+        <div className="flex row-span-3 border-2 border-red-500 w-60 h-60">
+          <span>New Cases {filteredDate.new_case}</span>
+          <span>Total {filteredDate.total_case}</span>
+        </div>
+        <div className="flex border-2 border-red-500">
+          <span>
+            Foreigner{" "}
+            {filteredDate.new_case - filteredDate.new_case_excludeabroad}
+          </span>
+          <span>
+            Total{" "}
+            {filteredDate.total_case - filteredDate.total_case_excludeabroad}
+          </span>
+        </div>
+        <div className="flex row-span-2 border-2 border-red-500">
+          <span>Thai {filteredDate.new_case_excludeabroad}</span>
+          <span>Total {filteredDate.total_case_excludeabroad}</span>
+        </div>
+        <div className="flex border-2 border-red-500">
+          <span>New Death {filteredDate.new_death}</span>
+          <span>Total Death {filteredDate.total_death} </span>
+        </div>
+        <div className="flex border-2 border-red-500">
+          <span>New Recovered {filteredDate.new_recovered}</span>
+          <span>Total Recovered {filteredDate.total_recovered} </span>
+        </div>
+      </div>
+    </div>
+      </div>
+      
+      
     </Layout>
-  )
-  }
+  );
+}
 
-export const getStaticProps = async () => {
-  const res = await fetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
-  const provinces = await res.json();
+export const getServerSideProps = async () => {
+  // const res = await fetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+  // const provinces = await res.json();
 
-  const allres = await fetch('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  const allres = await fetch(
+    "https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-all"
+  );
   const country = await allres.json();
-
-  const provincesData = provinces.reduce((acc,current) => {
-    const x = acc.find(item => item.province === current.province);
-    if (!x) {
-      return acc.concat([current]);
-    } else {
-      return acc;
-    }
-  }, [])
-
-
-  const countryData = country.reduce((acc,current) => {
-    const x = acc.find(item => item.txn_date === current.txn_date);
-    if (!x) {
-      return acc.concat([current]);
-    } else {
-      return acc;
-    }
-  }, [])
 
   return {
     props: {
-      provinces:provincesData,
-      country:countryData,
+      country: country,
     },
   };
 };
