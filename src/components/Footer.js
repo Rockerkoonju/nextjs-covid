78a80e448e90d962c475d110005f2ae6eec39e83
Rockerkoonju <78637372+Rockerkoonju@users.noreply.github.com>
2022-02-24 08:19:20 +0700

    Create-footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="fixed flex items-center justify-center font-Nunito text-secondary text-center bg-slate-800 w-full h-8 bottom-0">
+        Made by Rockerkoonju copyright 2022
+      </footer>
+    )
+}
+
+export default Footer
